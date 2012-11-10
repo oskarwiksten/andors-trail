b85943fa80fa0ce984022b380ef230d098b27435
Oskar Wiksten <oskar.wiksten@gmail.com>
2012-11-10 13:19:13 +0100

    Bump version number to v0.7.0dev

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/AndorsTrailApplication.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/AndorsTrailApplication.java
@@ -17,11 +17,11 @@ public final class AndorsTrailApplication extends Application {
 	public static final boolean DEVELOPMENT_FORCE_STARTNEWGAME = false;
 	public static final boolean DEVELOPMENT_FORCE_CONTINUEGAME = false;
 	public static final boolean DEVELOPMENT_DEBUGBUTTONS = false;
-	public static final boolean DEVELOPMENT_VALIDATEDATA = false;
-	public static final boolean DEVELOPMENT_DEBUGMESSAGES = false;
+	public static final boolean DEVELOPMENT_VALIDATEDATA = true;
+	public static final boolean DEVELOPMENT_DEBUGMESSAGES = true;
 	public static final boolean DEVELOPMENT_INCOMPATIBLE_SAVEGAMES = DEVELOPMENT_DEBUGRESOURCES;
-	public static final int CURRENT_VERSION = DEVELOPMENT_INCOMPATIBLE_SAVEGAMES ? 999 : 33;
-	public static final String CURRENT_VERSION_DISPLAY = "0.6.12";
+	public static final int CURRENT_VERSION = DEVELOPMENT_INCOMPATIBLE_SAVEGAMES ? 999 : 34;
+	public static final String CURRENT_VERSION_DISPLAY = "0.7.0dev";
 	
 	public final WorldContext world = new WorldContext();
 	public final WorldSetup setup = new WorldSetup(world, this);
