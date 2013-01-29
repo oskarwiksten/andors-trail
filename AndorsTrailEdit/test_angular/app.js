cc0ff7bdb17631a61179757765d4221d7281f774
Oskar Wiksten <oskar.wiksten@gmail.com>
2013-01-29 14:20:08 +0100

    Refactor content editor - Remove unused files.

--- AndorsTrailEdit/test_angular/app.js
+++
@@ -1,6 +0,0 @@
-angular
-	.module('oskar1', [])
-    .config(['$routeProvider', function($routeProvider) {
-		$routeProvider
-			.when('/item/:id', {templateUrl: 'detail.html', controller: DetailCtrl});
-	}]);
--- AndorsTrailEdit/test_angular/services.js
+++
@@ -1,8 +0,0 @@
-angular
-	.module('itemsService', ['ngResource'])
-	.factory('Item', function($resource) {
-		var items = [];
-		return $resource('phones/:phoneId.json', {}, {
-			query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-		});
-});
