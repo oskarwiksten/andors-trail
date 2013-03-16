626c558335ba458fbaa32ca573a868afad2ddb8a
Oskar Wiksten <oskar.wiksten@gmail.com>
2013-03-16 09:45:14 +0100

    Content editor: Move reusable logic functions for calculating item cost & monster exp to separate js file.

--- AndorsTrailEdit/js/controllers/monster.js
+++ AndorsTrailEdit/js/controllers/monster.js
@@ -1,33 +1,10 @@
-var ATEditor = (function(ATEditor, model) {
+var ATEditor = (function(ATEditor, model, ATModelFunctions) {
 
-	function getExperience(obj) {
-		var EXP_FACTOR_DAMAGERESISTANCE = 9;
-		var EXP_FACTOR_SCALING = 0.7;
-		
-		var div100 = function(v) { return v / 100; }
-		var v = function(i) { return i ? parseFloat(i) : 0; }
-		
-		var attacksPerTurn = Math.floor(v(obj.maxAP) / v(obj.attackCost));
-		var avgDamagePotential = 0;
-		if (obj.attackDamage) { avgDamagePotential = (v(obj.attackDamage.min) + v(obj.attackDamage.max)) / 2; }
-		var avgCrit = 0;
-		if (obj.hasCritical) {
-			avgCrit = div100(v(obj.criticalSkill)) * v(obj.criticalMultiplier);
-		}
-		var avgAttackHP  = attacksPerTurn * div100(v(obj.attackChance)) * avgDamagePotential * (1 + avgCrit);
-		var avgDefenseHP = v(obj.maxHP) * (1 + div100(v(obj.blockChance))) + EXP_FACTOR_DAMAGERESISTANCE * v(obj.damageResistance);
-		var attackConditionBonus = 0;
-		if (obj.hitEffect && obj.hitEffect.conditionsTarget && v(obj.hitEffect.conditionsTarget.length) > 0) {
-			attackConditionBonus = 50;
-		}
-		var experience = (avgAttackHP * 3 + avgDefenseHP) * EXP_FACTOR_SCALING + attackConditionBonus;
-		
-		return Math.ceil(experience);
-	};
-	
 	function MonsterController($scope, $routeParams) {
 		$scope.obj = model.monsters.findById($routeParams.id) || {};
-		$scope.getExperience = function() { return getExperience($scope.obj); }
+		$scope.getExperience = function() {
+			return ATModelFunctions.monsterFunctions.getMonsterExperience($scope.obj);
+		};
 		
 		$scope.addCondition = function(list) {
 			list.push({magnitude:1, duration:1, chance:100});
@@ -40,7 +17,7 @@ var ATEditor = (function(ATEditor, model) {
 	
 	function MonsterTableController($scope, $routeParams) {
 		$scope.monsters = model.monsters.items;
-		$scope.getExperience = getExperience;
+		$scope.getExperience = ATModelFunctions.monsterFunctions.getMonsterExperience;
 		$scope.edit = function(monster) {
 			window.location = "#/" + model.monsters.id + "/edit/" + monster.id;
 		};
@@ -55,4 +32,4 @@ var ATEditor = (function(ATEditor, model) {
 	ATEditor.controllers.MonsterTableController = MonsterTableController;
 
 	return ATEditor;
-})(ATEditor, ATEditor.model);
+})(ATEditor, ATEditor.model, ATModelFunctions);
