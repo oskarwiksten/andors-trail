98d2c89fed080ed56a3c25a004314751dd2159b2
Oskar Wiksten <oskar.wiksten@gmail.com>
2013-06-25 01:02:37 +0200

    Minor map fixes.

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/AndorsTrailApplication.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/AndorsTrailApplication.java
@@ -17,7 +17,7 @@ public final class AndorsTrailApplication extends Application {
 	public static final boolean DEVELOPMENT_DEBUGBUTTONS = false;
 	public static final boolean DEVELOPMENT_VALIDATEDATA = true;
 	public static final boolean DEVELOPMENT_DEBUGMESSAGES = true;
-	public static final boolean DEVELOPMENT_INCOMPATIBLE_SAVEGAMES = DEVELOPMENT_DEBUGRESOURCES;
+	public static final boolean DEVELOPMENT_INCOMPATIBLE_SAVEGAMES = DEVELOPMENT_DEBUGRESOURCES || DEVELOPMENT_DEBUGBUTTONS;
 	public static final int CURRENT_VERSION = DEVELOPMENT_INCOMPATIBLE_SAVEGAMES ? 999 : 36;
 	public static final String CURRENT_VERSION_DISPLAY = "0.7.0dev";
 	
