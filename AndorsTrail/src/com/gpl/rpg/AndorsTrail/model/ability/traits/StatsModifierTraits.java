b15cc3d827f978fffbdfd7044c2a41b61121d0b6
Todor Balabanov <todor.balabanov gmail com>
2013-11-27 14:17:15 +0200

    Code duplication in StatsModifierTraits class.
    Two identical functions were merged in a single privet helper function.

--- AndorsTrail/src/com/gpl/rpg/AndorsTrail/model/ability/traits/StatsModifierTraits.java
+++ AndorsTrail/src/com/gpl/rpg/AndorsTrail/model/ability/traits/StatsModifierTraits.java
@@ -7,7 +7,18 @@ public final class StatsModifierTraits {
 	public final VisualEffectCollection.VisualEffectID visualEffectID;
 	public final ConstRange currentHPBoost;
 	public final ConstRange currentAPBoost;
+	
+	private int calculateViolenceCost(final int hpValue, final int apValue) {
+		final float averageHPBoost = currentHPBoost == null ? 0 : currentHPBoost.averagef();
+		final float averageAPBoost = currentAPBoost == null ? 0 : currentAPBoost.averagef();
+		if (averageHPBoost == 0 && averageAPBoost == 0) return 0;
 
+		final int costBoostHP = (int) (hpValue * Math.pow(Math.max(0, averageHPBoost), 2.5) + 450 * averageHPBoost);
+		final int costBoostAP = (int) (apValue * Math.pow(Math.max(0, averageAPBoost), 2.5) + 300 * averageAPBoost);
+
+		return costBoostHP + costBoostAP;
+	}
+	 
 	public StatsModifierTraits(
 			VisualEffectCollection.VisualEffectID visualEffectID
 			, ConstRange currentHPBoost
@@ -31,22 +42,10 @@ public final class StatsModifierTraits {
 	}
 
 	public int calculateHitCost() {
-		final float averageHPBoost = currentHPBoost == null ? 0 : currentHPBoost.averagef();
-		final float averageAPBoost = currentAPBoost == null ? 0 : currentAPBoost.averagef();
-		if (averageHPBoost == 0 && averageAPBoost == 0) return 0;
-
-		final int costBoostHP = (int)(2770*Math.pow(Math.max(0,averageHPBoost), 2.5) + 450*averageHPBoost);
-		final int costBoostAP = (int)(3100*Math.pow(Math.max(0,averageAPBoost), 2.5) + 300*averageAPBoost);
-		return costBoostHP + costBoostAP;
+		return(calculateViolenceCost(2770,3100));
 	}
 
 	public int calculateKillCost() {
-		final float averageHPBoost = currentHPBoost == null ? 0 : currentHPBoost.averagef();
-		final float averageAPBoost = currentAPBoost == null ? 0 : currentAPBoost.averagef();
-		if (averageHPBoost == 0 && averageAPBoost == 0) return 0;
-
-		final int costBoostHP = (int)(923*Math.pow(Math.max(0,averageHPBoost), 2.5) + 450*averageHPBoost);
-		final int costBoostAP = (int)(1033*Math.pow(Math.max(0,averageAPBoost), 2.5) + 300*averageAPBoost);
-		return costBoostHP + costBoostAP;
+		return(calculateViolenceCost(923,1033));
 	}
 }
